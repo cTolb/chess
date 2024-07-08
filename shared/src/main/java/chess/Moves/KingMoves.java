915ef21bf25c72250e495df1fbace84d2306eb0f
Clark <ctolbert910@gmail.com>
2024-07-08 13:15:40 -0600

    changed king moves

--- shared/src/main/java/chess/Moves/KingMoves.java
+++ shared/src/main/java/chess/Moves/KingMoves.java
@@ -40,48 +40,39 @@ public class KingMoves {
         int currentRow = getPosition().getRow();
         int currentCol = getPosition().getColumn();
 
-        if (currentRow <= 7) {
-            ChessPosition newPosition = new ChessPosition(currentRow + 1, currentCol - 1);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
-            newPosition = new ChessPosition(currentRow + 1, currentCol);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
-            newPosition = new ChessPosition(currentRow + 1, currentCol + 1);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
+        ChessPosition newPosition = new ChessPosition(currentRow + 1, currentCol - 1);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
+        }
+        newPosition = new ChessPosition(currentRow + 1, currentCol);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
+        }
+        newPosition = new ChessPosition(currentRow + 1, currentCol + 1);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
+        }
+        newPosition = new ChessPosition(currentRow - 1, currentCol - 1);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
+        }
+        newPosition = new ChessPosition(currentRow - 1, currentCol);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
+        }
+        newPosition = new ChessPosition(currentRow - 1, currentCol + 1);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
+        }
+        newPosition = new ChessPosition(currentRow, currentCol + 1);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
+        }
+        newPosition = new ChessPosition(currentRow, currentCol - 1);
+        if (validMove(newPosition)) {
+            moves.add(new ChessMove(getPosition(), newPosition, null));
         }
 
-        if (currentRow >= 2) {
-            ChessPosition newPosition = new ChessPosition(currentRow - 1, currentCol - 1);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
-            newPosition = new ChessPosition(currentRow - 1, currentCol);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
-            newPosition = new ChessPosition(currentRow - 1, currentCol + 1);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
-        }
-
-        if (currentCol <= 7) {
-            ChessPosition newPosition = new ChessPosition(currentRow, currentCol + 1);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
-        }
-        if (currentCol >= 1) {
-            ChessPosition newPosition = new ChessPosition(currentRow, currentCol - 1);
-            if (validMove(newPosition)) {
-                moves.add(new ChessMove(getPosition(), newPosition, null));
-            }
-        }
         return moves;
     }
 
