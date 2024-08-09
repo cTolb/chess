665f5862102c3181179f763bca0d8fdd26e83658
Clark <ctolbert910@gmail.com>
2024-08-08 23:49:08 -0600

    created response files

---
+++ shared/src/main/java/model/LoginResponse.java
@@ -0,0 +1,4 @@
+package model;
+
+public record LoginResponse(AuthData authData, String message) {
+}
---
+++ shared/src/main/java/model/RegisterResponse.java
@@ -0,0 +1,4 @@
+package model;
+
+public record RegisterResponse(AuthData authData, String message) {
+}
